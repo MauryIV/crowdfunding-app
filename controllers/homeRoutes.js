91e7745ca0e6dfcc5ec74d513fe189997d824842
MauryIV <mauryhughesiv@gmail.com>
2024-03-30 14:43:15 -0600

    created the homepage through handlebars and edited homepage controller appropriately.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const { Project } = require('../models')
 
 router.get('/', async (req, res) => {
-  res.render('homepage');
-})
+  try{
+    const projectData = await Project.findAll({
+      order: [['name', 'ASC']]
+    });
+    const projects = projectData.map((project) => project.get({ plain: true }));
+    res.render('homepage', { projects });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
@@ -14,14 +17,18 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
